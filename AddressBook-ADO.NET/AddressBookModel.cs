cede97dbbad34c9e9d47c750cd86e634ef59ce43
ekta12345 <ektakumbhare3@gmail.com>
2021-05-30 00:18:59 +0530

    Added:Creating a database and connecting the database

--- AddressBook-ADO.NET/AddressBookModel.cs
+++ AddressBook-ADO.NET/AddressBookModel.cs
@@ -8,5 +8,16 @@ namespace AddressBook_ADO.NET
 {   
     public class AddressBookModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+        public string AddressBookType { get; set; }
+        public string AddressBookName { get; set; }
+
     }
 }
