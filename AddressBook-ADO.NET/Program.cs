08447bc124f384a46b7d75a185ad0b6ad41a992f
ekta12345 <ektakumbhare3@gmail.com>
2021-05-30 00:34:57 +0530

    Added:Ability to get all contcts from the table

--- AddressBook-ADO.NET/Program.cs
+++ AddressBook-ADO.NET/Program.cs
@@ -12,7 +12,8 @@ namespace AddressBook_ADO.NET
         {
             Console.WriteLine("Welcome Address Book System ADO.NET");
             AddressBookManagement addressBookManagement = new AddressBookManagement();
-            addressBookManagement.DataBaseConnection(); //UC1
+            //addressBookManagement.DataBaseConnection(); //UC1
+            addressBookManagement.GetAllContact(); //UC2
             Console.ReadLine();
 
         }  
